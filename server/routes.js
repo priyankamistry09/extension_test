3b059c1e9fbcba898967e28cab1065cb1031cede
abhijeet-sonawane <abhijeet.sonawane@raweng.com>
2017-02-28 11:31:17 +0530

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,7 @@ module.exports = {
 	"/v1/functions/smita": {
 		GET: function(req, res){
 			this.resSuccess(req, res, {
-				smita: "is new update :D"
+				smita: "is new updateeeee :D"
 			})
 		}
 	},
