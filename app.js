46d7514eec019caf7179556be5eb0198d6a0740d
Priyanka Mistry <priyankamistry09@users.noreply.github.com>
2018-02-08 14:47:37 +0530

    Update app.js
    updated api key and master key

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('bltd470ad1e7ce19b96')
+var app = Built.App('blt1387b1f751b602e0')
 // .setHost('localhost')
 // .setPort(8000)
 .setHost("test-stag-api.built.io")
 .setProtocol("https")
 // .setPort(80)
-.setMasterKey('bltd6b6eedeff10f9b0')
+.setMasterKey('blt8bcfca5f57ba5314')
 
 var extensionSDK = app.Extension({
 	secret_key     : 'altamash',
